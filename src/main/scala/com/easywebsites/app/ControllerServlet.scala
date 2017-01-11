6c9f464fb76ff706ddb0fa0d983c23f64bee39ac
propro1994 <prosperemmanu.mbadibayebec@epita.fr>
2017-01-11 22:20:36 +0000

    add of all the views

--- src/main/scala/com/easywebsites/app/ControllerServlet.scala
+++ src/main/scala/com/easywebsites/app/ControllerServlet.scala
@@ -13,4 +13,32 @@ class ControllerServlet extends EasywebsitesappStack {
     </html>
   }
 
+  get("/newUser"){
+  contentType="text/html"
+  ssp("/newUser")
+  }
+
+  get("/connection"){
+  contentType="text/html"
+  ssp("/connection")
+  }
+
+  post("/connection"){
+  contentType="text/html"
+
+ 
+  email := params.get("email")
+  password := params.get("password")
+  //get result of authentification
+  result := model.Connection(email, password)
+  if result {
+    ctx.Session().Set("isConnected", "true")
+    ctx.Redirect("/connect/")
+  } else {
+    ctx.Render("connection_error.html", nil)
+  }
+
+  ssp("/connection")
+  }
+
 }

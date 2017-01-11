f19ba8c24a9cb9fe92905cc565b3d35ae4911e9c
Nicolas Drouin <nicolas.drouin@epita.fr>
2017-01-11 19:24:05 +0100

    fixed copy sources

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+cp -rf /root/nlpf-tp2/src /root/easywebsitesapp/
 ./sbt package
 cp -rf /root/easywebsitesapp/target/scala-2.12/easywebsitesapp_2.12-1.0.war /var/lib/jetty/webapps/root.war
 jetty.sh restart

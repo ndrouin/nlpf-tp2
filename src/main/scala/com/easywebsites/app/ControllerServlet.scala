1e4dbde12008c3d7f307ddf2ee27d691c3ee3772
Nicolas Drouin <nicolas.drouin@epita.fr>
2017-01-11 18:08:16 +0100

    added identifier production

--- src/main/scala/com/easywebsites/app/ControllerServlet.scala
+++ src/main/scala/com/easywebsites/app/ControllerServlet.scala
@@ -7,7 +7,7 @@ class ControllerServlet extends EasywebsitesappStack {
   get("/") {
     <html>
       <body>
-        <h1>Hello, world!</h1>
+        <h1>Production</h1>
         Say <a href="hello-scalate">hello to Scalate</a>.
       </body>
     </html>
